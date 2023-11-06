4edf8ef417450346c10dbfcf36fda21a232afd59
akshank <singalaakshank98@gmail.com>
2023-11-06 19:06:39 +0530

    program completed

---
+++ MYSELF.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main();
+{
+clrscr();
+printf("my name is akshank...\n");
+printf("my age 18...\n");
+printf("my school name is ashadeep iit\n");
+printf("my coures is flutter...\n") ;
+printf("my gr id is 4349\n");
+printf("i am living in surat\n");
+getch();
+}
